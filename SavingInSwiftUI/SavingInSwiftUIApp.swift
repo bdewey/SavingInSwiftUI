e5bc10cabcdffc3bbe7a31dd2055fec11b6302f0
Brian Dewey <bdewey@gmail.com>
2022-09-11 20:37:21 -0700

    Initial Commit

---
+++ SavingInSwiftUI/SavingInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingInSwiftUIApp.swift
+//  SavingInSwiftUI
+//
+//  Created by Brian Dewey on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SavingInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
